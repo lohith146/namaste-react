b4d9fc64d380dbc7c2c1a45129e832a670a1d547
Lohith Sindiri <lohithsindiri@MacBook-Air.local>
2023-10-19 23:46:36 +0530

    Revert "Revert "Revert "Revert "namaste react episode 1""""
    This reverts commit 9ca610306ee7e8cfc6d0fdd6b190595fd37a42c1.
    
    	notes-ep-1
    i
    git add notes-ep-1

--- index.js
+++
@@ -1,9 +0,0 @@
-const heading = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h4", { id: "heading4" }, "Heading4"),
-    React.createElement("h5", { id: "heading5" }, "Heading5"),
-  ]),
-  React.createElement("div", { id: "child2" }, "I am a child2"),
-]);
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
