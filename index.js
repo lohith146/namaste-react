333e15424866c7f51588ea9955b58cc8bd4c37f2
lohith146 <lohith.146@gmail.com>
2023-10-24 21:36:39 +0530

    Removed cdn links and installed via npm

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 const heading = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
     React.createElement("h4", { id: "heading4" }, "Heading4"),
