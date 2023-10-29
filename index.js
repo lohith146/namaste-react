4c3a9af2c276776c5b1029ef0942266af9f2bfd6
lohith146 <lohith.146@gmail.com>
2023-10-29 15:44:01 +0530

    Added fonts and components

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-const heading = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h4", { id: "heading4" }, "Heading4"),
-    React.createElement("h5", { id: "heading5" }, "Heading5"),
-  ]),
-  React.createElement("div", { id: "child2" }, "I am a child2"),
-]);
+
+const Header = () => (
+  <div className="header">
+    <div className="logo">
+      <img src="https://png.pngtree.com/png-vector/20220708/ourmid/pngtree-fast-food-logo-png-image_5763171.png" />
+    </div>
+    <div className="nav-items">
+      <ul>
+        <li>Home</li>
+        <li>About Us</li>
+        <li>Contact Us</li>
+        <li>Cart</li>
+      </ul>
+    </div>
+  </div>
+);
+
+const Card = () => <div className="card">Card</div>;
+
+const Body = () => (
+  <div>
+    <div className="search">Search</div>
+    <div className="res-container">
+      <Card />
+    </div>
+  </div>
+);
+const App = () => (
+  <div className="app">
+    <Header />
+    <Body />
+  </div>
+);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(<App />);
